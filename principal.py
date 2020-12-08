7e1c6ab0c8f11e1ed528f18cb19f8dc0decb1bfa
taleshrocha <taleshrochaz@gmail.com>
2020-12-08 18:34:11 -0300

    Codigo-comentado

--- principal.py
+++ principal.py
@@ -1,13 +1,10 @@
-quantidade = int(input("Qual é a quantidade de horários que você quer colocar? "))
-lista_horario = []
-
-for i in range(0, quantidade):
-	valor = input("Digite o "+str(i+1)+"º horário: ")
-	lista_horario.append(valor)
-
-
-
-
-
+#quantidade = int(input("Qual é a quantidade de horários que você quer colocar? "))
+#lista_horario = []
+#
+#for i in range(0, quantidade):
+#	valor = input("Digite o "+str(i+1)+"º horário: ")
+#	lista_horario.append(valor)
 # USAR DEPOIS
 # dados = open('dados.txt', 'r')
+
+
